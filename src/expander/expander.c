ee3e6a7dedcf8daee312746888a368d8e477e96a
bumkand <jakub.andrasi@gmail.com>
2025-06-04 22:44:44 +0200

    expander

--- src/expander/expander.c
+++ src/expander/expander.c
@@ -6,7 +6,11 @@
 /*   By: hoskim <hoskim@student.42prague.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/25 17:02:37 by jakand            #+#    #+#             */
+<<<<<<< HEAD
 /*   Updated: 2025/06/04 10:00:47 by hoskim           ###   ########seoul.kr  */
+=======
+/*   Updated: 2025/05/29 22:50:49 by jakand           ###   ########.fr       */
+>>>>>>> 518953b (expander)
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,8 +39,140 @@ char	*erase_dollar(const char *s)
 	return (dup);
 }
 
+char    *end_of_token(char *arg, int j)
+{
+    int     i;
+    int     k;
+    char    *ptr;
+    
+    i = j;
+    while(arg[i] != '\0')
+        i++;
+    i = i - j;
+    ptr = malloc((i + 1) * sizeof(char));
+    if (!ptr)
+        return (NULL);
+    k = 0;
+    while (j < i)
+    {
+        ptr[k] = arg[j];
+        k++;
+        j++;
+    }
+    ptr[k] = '\0';
+    return (ptr);
+}
+
+char    *make_var(char *arg, int start, int end, int len)
+{
+    char    *ptr;
+
+    ptr = malloc(len * sizeof(char));
+    if (!ptr)
+        return (NULL);
+    start++;
+    len = 0;
+    while (start < end)
+    {
+        ptr[len] = arg[start];
+        start++;
+        len++;
+    }
+    ptr[len] = '\0';
+    return (ptr);
+}
+
+char    *start_of_token(char *arg)
+{
+    int     i;
+    int     j;
+    char    *ptr;
+
+    i = 0;
+    while (arg[i] != '$')
+        i++;
+    ptr = malloc((i + 1) * sizeof(char));
+    if (!ptr)
+        return (NULL);
+    j = 0;
+    while (j < i)
+    {
+        ptr[j] = arg[i];
+        i++;
+        j++;
+    }
+    ptr[j] = '\0';
+    return (ptr);
+}
+
+char    *remake_token(char *start, char *var, char *end)
+{
+    int     i;
+    int     j;
+    int     k;
+    char    *ptr;
+
+    i = ft_strlen(start) + ft_strlen(var) + ft_strlen(end);
+    ptr = malloc((i + 1) * sizeof(char));
+    if (!ptr)
+        return (NULL);
+    j = 0;
+    while (j < i && start[j])
+    {
+        ptr[j] = start[j];
+        j++;
+    }
+    k = 0;
+    while (j < i && var[k])
+    {
+        ptr[j] = var[k];
+        j++;
+        k++;
+    }
+    k = 0;
+    while (j < i && end[k])
+    {
+        ptr[j] = end[k];
+        j++;
+        k++;
+    }
+    ptr[j] = '\0';
+    return (ptr);
+}
+
+void    expand_double_quotes(char **arg)
+{
+    int     i;
+    int     j;
+    char    *ptr;
+    char    *var;
+    char    *varvar;
+    char    *start;
+
+    i = 0;
+    while ((*arg)[i])
+    {
+        if ((*arg)[i] == '$')
+        {
+            j = i;
+            while ((*arg)[i] != '\0' && (*arg)[i] != 9 && (*arg)[i] != 13 && (*arg)[i] != ' ')
+                i++;
+            start = start_of_token(*arg);
+            ptr = end_of_token(*arg, j);
+            var = make_var(*arg, j, i, (i - j));
+            varvar = getenv(var);
+            printf("%s\n", varvar);
+            if (var)
+                free(var);
+            free((*arg));
+            (*arg) = remake_token(start, varvar, ptr);
+        }
+    }
+}
+
 void    expand_token(t_command *token)
 {
+<<<<<<< HEAD
 	int i;
 	char    *env;
 
@@ -57,4 +193,33 @@ void    expand_token(t_command *token)
 		}
 		token = token->next;
 	}
+=======
+    int i;
+    char    *env;
+    char    *original;
+
+    env = NULL;
+    while(token)
+    {
+        i = 0;
+        while (token->args && token->args[i] != NULL)
+        {
+            if (token->args[i][0] == '$' && token->args_types[i] == 1)
+            {
+                original = token->args[i];  // Save the pointer
+                env = erase_dollar(original);  // Extract variable name
+                token->args[i] = ft_strdup(getenv(env));  // Get and duplicate env value
+                free(original);  // Free the original
+                free(env);  // Free the variable name
+            }
+            else if (token->args_types[i] == 2)
+            {
+                expand_double_quotes(&token->args[i]);
+            }
+            // For other types, keep the string as is (don't free it)
+            i++;
+        }
+        token = token->next;
+    }
+>>>>>>> 518953b (expander)
 }
