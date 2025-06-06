8c3bdcdd5d7390eee045500675dea11054f17f3e
bumkand <jakub.andrasi@gmail.com>
2025-06-06 23:46:56 +0200

    expand almost done

--- src/expander/expander.c
+++ src/expander/expander.c
@@ -6,7 +6,7 @@
 /*   By: jakand <jakand@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/25 17:02:37 by jakand            #+#    #+#             */
-/*   Updated: 2025/06/06 00:51:14 by jakand           ###   ########.fr       */
+/*   Updated: 2025/06/06 23:39:25 by jakand           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -102,77 +102,77 @@ char    *start_of_env(char *arg)
     return (ptr);
 }
 
-char    *remake_token(char *start, char *var, char *end)
-{
-    int     i;
-    int     j;
-    int     k;
-    char    *ptr;
+// char    *remake_token(char *start, char *var, char *end)
+// {
+//     int     i;
+//     int     j;
+//     int     k;
+//     char    *ptr;
 
-    i = ft_strlen(start) + ft_strlen(var) + ft_strlen(end);
-    ptr = malloc((i + 1) * sizeof(char));
-    if (!ptr)
-        return (NULL);
-    j = 0;
-    while (j < i && start[j])
-    {
-        ptr[j] = start[j];
-        j++;
-    }
-    k = 0;
-    while (j < i && var[k])
-    {
-        ptr[j] = var[k];
-        j++;
-        k++;
-    }
-    k = 0;
-    while (j < i && end[k])
-    {
-        ptr[j] = end[k];
-        j++;
-        k++;
-    }
-    ptr[j] = '\0';
-    return (ptr);
-}
+//     i = ft_strlen(start) + ft_strlen(var) + ft_strlen(end);
+//     ptr = malloc((i + 1) * sizeof(char));
+//     if (!ptr)
+//         return (NULL);
+//     j = 0;
+//     while (j < i && start[j])
+//     {
+//         ptr[j] = start[j];
+//         j++;
+//     }
+//     k = 0;
+//     while (j < i && var[k])
+//     {
+//         ptr[j] = var[k];
+//         j++;
+//         k++;
+//     }
+//     k = 0;
+//     while (j < i && end[k])
+//     {
+//         ptr[j] = end[k];
+//         j++;
+//         k++;
+//     }
+//     ptr[j] = '\0';
+//     return (ptr);
+// }
 
-void    expand_double_quotes(char **arg)
-{
-    int     i;
-    int     j;
-    char    *ptr;
-    char    *var;
-    char    *varvar;
-    char    *start;
+// void    expand_double_quotes(char **arg)
+// {
+//     int     i;
+//     int     j;
+//     char    *ptr;
+//     char    *var;
+//     char    *varvar;
+//     char    *start;
 
-    i = 0;
-    while ((*arg)[i])
-    {
-        if ((*arg)[i] == '$')
-        {
-            j = i;
-            while ((*arg)[i] != '\0' && (*arg)[i] != 9 && (*arg)[i] != 13 && (*arg)[i] != ' ')
-                i++;
-            start = start_of_env(*arg);
-            printf("start=> %s\n", start);
+//     i = 0;
+//     while ((*arg)[i])
+//     {
+//         if ((*arg)[i] == '$')
+//         {
+//             j = i;
+//             while ((*arg)[i] != '\0' && (*arg)[i] != 9 && (*arg)[i] != 13 && (*arg)[i] != ' ')
+//                 i++;
+//             start = start_of_env(*arg);
+//             printf("start=> %s\n", start);
             
-            ptr = environment_variable(*arg, j);
-            printf("ptr=> %s\n", ptr);
+//             ptr = environment_variable(*arg, j);
+//             printf("ptr=> %s\n", ptr);
             
-            var = make_var(*arg, j, i, (i - j));
-            printf("var=> %s\n", var);
+//             var = make_var(*arg, j, i, (i - j));
+//             printf("var=> %s\n", var);
             
-            varvar = getenv(var);
-            printf("varvar=> %s\n", varvar);
+//             varvar = getenv(var);
+//             printf("varvar=> %s\n", varvar);
             
-            if (var)
-                free(var);
-            free((*arg));
-            (*arg) = remake_token(start, varvar, ptr);
-        }
-    }
-}
+//             if (var)
+//                 free(var);
+//             free((*arg));
+//             (*arg) = remake_token(start, varvar, ptr);
+//         }
+//     }
+// }
 
 void    update_token(char **arg, char *start, int j)
 {
@@ -191,6 +191,43 @@ void    update_token(char **arg, char *start, int j)
     {
         ptr[l] = start[l];
         l++;
+    }
+    while (l < i && (*arg)[j])
+    {
+        ptr[l] = (*arg)[j];
+        l++;
+        j++;
+    }
+    ptr[l] = '\0';
+    free((*arg));
+    (*arg) = ptr;
+}
+
+void    remake_token(char **arg, char *start, char *var, int j)
+{
+    int     i;
+    int     l;
+    int     m;
+    char    *ptr;
+
+    while ((*arg)[j] != '\0' && !((*arg)[j] >= 9 && (*arg)[j] <= 13) && (*arg)[j] != ' ')
+        j++;
+    i = ft_strlen(start) + ft_strlen(var) + (ft_strlen(*arg) - j);
+    ptr = malloc((i + 1) * sizeof(char));
+    if (!ptr)
+        return ;
+    l = 0;
+    while (l < i && start[l])
+    {
+        ptr[l] = start[l];
+        l++;
+    }
+    m = 0;
+    while (l < i && var[m])
+    {
+        ptr[l] = var[m];
+        l++;
+        m++;
     }
     while (l < i && (*arg)[j])
     {
@@ -224,7 +261,7 @@ void    expand_token(t_command *token)
                 j++;
             if (token->args[i][j] == '$' && token->args_types[i] != 0)
             {
-                original = token->args[i];  // Save the pointer
+                original = token->args[i];
                 start = start_of_env(original);
                 printf("startttt=> %s\n", start);
                 env = environment_variable(original, j);
@@ -245,6 +282,13 @@ void    expand_token(t_command *token)
                     free(env);
                     free(start);
                     continue ;
+                }
+                else if (var && token->args_types[i] == 2)
+                {
+                    remake_token(&token->args[i], start, var, j);
+                    free(env);
+                    free(start);
+                    continue;
                 }
                 // env = erase_dollar(original);  // Extract variable name
                 token->args[i] = ft_strdup(getenv(env));  // Get and duplicate env value
