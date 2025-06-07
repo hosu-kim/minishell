0dbafd2f3099b7ca89709f11a0d781103b55950a
bumkand <jakub.andrasi@gmail.com>
2025-06-08 00:08:01 +0200

    lines are good

--- src/expander/expander.c
+++ src/expander/expander.c
@@ -6,7 +6,7 @@
 /*   By: jakand <jakand@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/25 17:02:37 by jakand            #+#    #+#             */
-/*   Updated: 2025/06/07 19:25:44 by jakand           ###   ########.fr       */
+/*   Updated: 2025/06/08 00:07:24 by jakand           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -119,14 +119,47 @@ void    remake_token(char **arg, char *start, char *var, int j)
     (*arg) = ptr;
 }
 
+void	free_var(char **env, char **var)
+{
+	if (*env)
+		free(*env);
+	if (*var)
+		free(*var);
+}
+
+int	make_env(t_command **token, int i, int j)
+{
+	char    *env;
+    char    *var;
+    char    *start;
+
+	start = start_of_env((*token)->args[i]);
+    env = environment_variable((*token)->args[i], j);
+    var = getenv(env);
+    printf("startttt=> %s\nenvvvvv=> %s\nvarrrr=> %s\n", start, env, var);
+    if (var == NULL && (*token)->args_types[i] < 3)
+    {
+		update_token(&(*token)->args[i], start, j);
+		if ((*token)->args[i][0] == '\0')
+			(*token)->args_types[i] = 3;
+		free_var(&env, &start);
+        return (1);
+    }
+    else if (var)
+    {
+        remake_token(&(*token)->args[i], start, var, j);
+		free_var(&env, &start);
+        return (1);
+    }
+	free_var(&env, &start);
+	return (0);
+}
+
 void    expand_token(t_command *token)
 {
     int i;
     int j;
-    char    *env;
-    char    *var;
-    char    *start;
-
+	
     while(token)
     {
         i = 0;
@@ -138,37 +171,8 @@ void    expand_token(t_command *token)
                 j++;
             if (token->args[i][j] == '$' && token->args_types[i] != 0)
             {
-                start = start_of_env(token->args[i]);
-                printf("startttt=> %s\n", start);
-                env = environment_variable(token->args[i], j);
-                printf("envvvvv=> %s\n", env);
-                var = getenv(env);
-                printf("varrrr=> %s\n", var);
-                if (var == NULL && token->args_types[i] == 1)
-                {
-                    token->args_types[i] = 3;
-                    i++;
-                    free(env);
-                    free(start);
-                    continue ;
-                }
-                else if (var == NULL && token->args_types[i] == 2)
-                {
-                    update_token(&token->args[i], start, j);
-                    free(env);
-                    free(start);
-                    continue ;
-                }
-                else if (var && token->args_types[i] == 2)
-                {
-                    remake_token(&token->args[i], start, var, j);
-                    free(env);
-                    free(start);
-                    continue ;
-                }
-                remake_token(&token->args[i], start, var, j);
-                free(env);
-                free(start);
+                if (make_env(&token, i, j) == 1)
+					continue ;
             }
             i++;
         }
