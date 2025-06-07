a7aac30094b02c84f5e65956b0b62fe8aeebfbec
hosu-kim <hosu@outlook.cz>
2025-06-07 16:34:04 +0200

    refactor: Update author information and format code for consistency in free_command.c and parser.h

--- src/parser/free_command.c
+++ src/parser/free_command.c
@@ -3,79 +3,79 @@
 /*                                                        :::      ::::::::   */
 /*   free_command.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jakand <jakand@student.42.fr>              +#+  +:+       +#+        */
+/*   By: hoskim <hoskim@student.42prague.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/25 13:29:59 by jakand            #+#    #+#             */
-/*   Updated: 2025/05/25 20:39:50 by jakand           ###   ########.fr       */
+/*   Updated: 2025/06/07 16:22:14 by hoskim           ###   ########seoul.kr  */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "parser.h"
 
-void    free_args(t_command *start)
+void	free_args(t_command *start)
 {
-    int     i;
+	int	i;
 
-    i = 0;
-    if (start->args)
-    {
-        i = 0;
-        while (start->args[i])
-        {
-            free(start->args[i]);
-            i++;
-        }
-        free(start->args);
-    }
-    if (start->args_types)
-        free(start->args_types);
+	i = 0;
+	if (start->args)
+	{
+		i = 0;
+		while (start->args[i])
+		{
+			free(start->args[i]);
+			i++;
+		}
+		free(start->args);
+	}
+	if (start->args_types)
+		free(start->args_types);
 }
 
-void    free_input_redir(t_redirection *redir)
+void	free_input_redir(t_redirection *redir)
 {
-    t_redirection *temp;
-    
-    while (redir)
-    {
-        temp = redir->next;
-        free(redir->target);
-        free(redir);
-        redir = temp;
-    }
+	t_redirection *temp;
+	
+	while (redir)
+	{
+		temp = redir->next;
+		free(redir->target);
+		free(redir);
+		redir = temp;
+	}
 }
 
-void    free_output_redir(t_redirection *redir)
+void	free_output_redir(t_redirection *redir)
 {
-    t_redirection *temp;
-    
-    while (redir)
-    {
-        temp = redir->next;
-        free(redir->target);
-        free(redir);
-        redir = temp;
-    }
+	t_redirection *temp;
+	
+	while (redir)
+	{
+		temp = redir->next;
+		free(redir->target);
+		free(redir);
+		redir = temp;
+	}
 }
 
 void    free_command(t_command *start)
 {
-    t_command       *cmd;
-    t_redirection   *redir;
+	t_command       *cmd;
+	t_redirection   *redir;
 
-    if (!start)
-        return ;
-    cmd = start;
-    while (start)
-    {
-        cmd = start->next;
-        free_args(start);
-        redir = start->input_redir;
-        if (redir)
-            free_input_redir(redir);
-        redir = start->output_redir;
-        if (redir)
-            free_output_redir(redir);
-        free(start);
-        start = cmd;
-    }
+	if (!start)
+		return ;
+	cmd = start;
+	while (start)
+	{
+		cmd = start->next;
+		free_args(start);
+		redir = start->input_redir;
+		if (redir)
+			free_input_redir(redir);
+		redir = start->output_redir;
+		if (redir)
+			free_output_redir(redir);
+		free(start);
+		start = cmd;
+	}
 }
--- src/parser/parser.h
+++ src/parser/parser.h
@@ -6,7 +6,7 @@
 /*   By: hoskim <hoskim@student.42prague.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/15 20:07:30 by jakand            #+#    #+#             */
-/*   Updated: 2025/06/04 21:47:26 by hoskim           ###   ########seoul.kr  */
+/*   Updated: 2025/06/07 16:21:58 by hoskim           ###   ########seoul.kr  */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,52 +17,52 @@
 
 typedef enum e_redir_type
 {
-    TOK_INPUT,		// <
-	TOK_OUTPUT,	    // >
-	TOK_APPEND,	    // >>
-    TOK_HEREDOC		// <<
-}   t_redir_type;
+	TOK_INPUT,  // <
+	TOK_OUTPUT, // >
+	TOK_APPEND, // >>
+	TOK_HEREDOC // <<
+}	t_redir_type;
 
 typedef struct s_redirection
 {
-    t_redir_type            type;           // redirection type
-    char                    *target;        // target file or heredoc delimiter
-    int                     target_types;   // types of words (quotes - 0, double quotes - 2, no quotes - 1)
-    struct s_redirection    *next;          // pointer to next redirection
-}   t_redirection;
+	t_redir_type			type;         // redirection type
+	char					*target;      // target file or heredoc delimiter
+	int						target_types; // types of words (quotes - 0, double quotes - 2, no quotes - 1)
+	struct s_redirection	*next;        // pointer to next redirection
+}	t_redirection;
 
 typedef struct s_command
 {
-    char                **args;             // args and command on 0 position
-    int                 argc;                // amount of arguments
-    int                 *args_types;        // types of words (quotes - 0, double quotes - 2, no quotes - 1)
-    t_redirection       *input_redir;       // list of input redirections
-    t_redirection       *output_redir;      // list of output redirections
-    int                 pipe;               // 1 pipe, 0 no pipe
-    struct s_command    *next;              // if pipe pointer to next command
-}   t_command;
+	char				**args;        // args and command on 0 position
+	int					argc;          // amount of arguments
+	int					*args_types;   // types of words (quotes - 0, double quotes - 2, no quotes - 1)
+	t_redirection		*input_redir;  // list of input redirections
+	t_redirection		*output_redir; // list of output redirections
+	int					pipe;          // 1 pipe, 0 no pipe
+	struct s_command	*next;         // if pipe pointer to next command
+}	t_command;
 
 // prepare lexed tokens for execution
 t_command	*parser(t_token *start);
 
 // make word tokens for execution
-int     pars_words(t_token *lex_start, t_token **lex_token,
-        t_command *new_token);
+int			pars_words(t_token *lex_start, t_token **lex_token,
+		t_command *new_token);
 
 // make pipe tokens for execution
-int     pipe_tok(t_token **lex_token, t_command *new_token);
+int			pipe_tok(t_token **lex_token, t_command *new_token);
 
 // make redir tokens for execution
-int     redir_tok(t_token **lex_token, t_command *new_token);
+int			redir_tok(t_token **lex_token, t_command *new_token);
 
 // get type (word, quotes, double quotes)
-int     get_args_type(int type);
+int			get_args_type(int type);
 
 // free parsed tokens in main
-void	free_token_parsed(t_command *token_parsed);
+void		free_token_parsed(t_command *token_parsed);
 
 // free parsed tokens in parser
-void    free_command(t_command *start);
+void		free_command(t_command *start);
 
 
 #endif
