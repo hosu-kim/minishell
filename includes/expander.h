97c1d00dbe855fb44427be2f1c387fbaa913f271
hosu-kim <hosu@outlook.cz>
2025-06-21 17:45:23 +0200

    Update header comments and reorganize function declarations in expander.h

--- includes/expander.h
+++ includes/expander.h
@@ -6,7 +6,7 @@
 /*   By: hoskim <hoskim@student.42prague.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/25 17:02:55 by jakand            #+#    #+#             */
-/*   Updated: 2025/06/21 13:21:14 by hoskim           ###   ########seoul.kr  */
+/*   Updated: 2025/06/21 17:44:33 by hoskim           ###   ########seoul.kr  */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,22 +18,13 @@
 typedef struct s_cmd_token	t_cmd_token;
 
 int		find_var_end(char *arg, int start);
-
-// Make expander for $
-void	expand_token(t_cmd_token *token, int exit_status);
-
-// Updating token
-void	update_token(char **arg, char *start, int j);
-
-// Remaking token
-void	remake_token(char **arg, char *start, char *var, int j);
-
-// Helper functions
-int		is_valid_var_char(char c);
-void	process_token_args(t_cmd_token *token, int exit_status);
-char	*get_variable_value(char *env, int exit_status);
-int		make_env(t_cmd_token **token, int i, int j, int exit_status);
-char	*extract_var_name(char *arg, int start, int end);
 char	*start_of_env(char *arg);
+char	*extract_var_name(char *arg, int start, int end);
+int		make_env(t_cmd_token **token, int i, int j, int exit_status);
+void	expand_token(t_cmd_token *token, int exit_status);
+char	*get_variable_value(char *env, int exit_status);
+void	process_token_args(t_cmd_token *token, int exit_status);
+void	update_token(char **arg, char *start, int j);
+void	remake_token(char **arg, char *start, char *var, int j);
 
 #endif
