d74acf495b06c6b90396004863f22acb0653a81c
hosu-kim <hosu@outlook.cz>
2025-06-21 20:28:06 +0200

    Implement identifier validation in export and unset builtins

--- src/builtin/export.c
+++ src/builtin/export.c
@@ -6,11 +6,32 @@
 /*   By: hoskim <hoskim@student.42prague.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/19 21:06:25 by hoskim            #+#    #+#             */
-/*   Updated: 2025/06/21 02:01:42 by hoskim           ###   ########seoul.kr  */
+/*   Updated: 2025/06/21 19:13:13 by hoskim           ###   ########seoul.kr  */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "builtin.h"
+
+static int	is_valid_identifier(const char *str)
+{
+	int	i;
+
+	if (!str || !*str)
+		return (NO);
+	if (!((*str >= 'a' && *str <= 'z') || (*str >= 'A' && *str <= 'Z'))
+		&& *str != '_')
+		return (NO);
+	i = 1;
+	while (str[i])
+	{
+		if (!((str[i] >= 'a' && str[i] <= 'z')
+			|| (str[i] >= 'A' && str[i]<= 'Z')
+			|| (str[i] >= '0' && str[i] <= '9')) && str[i] != '_')
+			return (NO);
+		i++;
+	}
+	return (YES);
+}
 
 static char	*extract_key(char *arg, char *address_of_equal_sign)
 {
@@ -32,7 +53,15 @@ static char	*extract_key(char *arg, char *address_of_equal_sign)
 	return (key);
 }
 
-static void	process_export_arg(char *arg, char ***env)
+static int	handle_export_error(char *arg)
+{
+	write(STDERR_FILENO, "minishell: export: `", 20);
+	write(STDERR_FILENO, arg, ft_strlen(arg));
+	write(STDERR_FILENO, "': not a valid identifier\n", 26);
+	return (YES);
+}
+
+static int	process_export_arg(char *arg, char ***env)
 {
 	char	*address_of_equal_sign;
 	char	*key;
@@ -42,27 +71,40 @@ static void	process_export_arg(char *arg, char ***env)
 	if (address_of_equal_sign)
 	{
 		key = extract_key(arg, address_of_equal_sign);
-		if (key)
+		if (!key || !is_valid_identifier(key))
 		{
-			value = ft_strdup(address_of_equal_sign + 1);
-			*env = add_env_var(*env, key, value);
-			free(key);
-			free(value);
+			if (key)
+				free(key);
+			return (handle_export_error(arg));
 		}
+		value = ft_strdup(address_of_equal_sign + 1);
+		*env = add_env_var(*env, key, value);
+		free(key);
+		free(value);
 	}
+	else
+	{
+		if (!is_valid_identifier(arg))
+			return (handle_export_error(arg));
+		*env = add_env_var(*env, arg, "");
+	}
+	return (SUCCESS);
 }
 
 int	builtin_export(char **args, char ***env)
 {
 	int	i;
+	int	exit_code;
 
 	if (!args[1])
 		return (builtin_env(*env));
+	exit_code = 0;
 	i = 1;
 	while (args[i])
 	{
-		process_export_arg(args[i], env);
+		if (process_export_arg(args[i], env) == 1)
+		exit_code = 1;
 		i++;
 	}
-	return (SUCCESS);
+	return (exit_code);
 }
--- src/builtin/unset.c
+++ src/builtin/unset.c
@@ -6,11 +6,32 @@
 /*   By: hoskim <hoskim@student.42prague.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/19 21:12:03 by hoskim            #+#    #+#             */
-/*   Updated: 2025/06/19 21:46:56 by hoskim           ###   ########seoul.kr  */
+/*   Updated: 2025/06/21 19:34:34 by hoskim           ###   ########seoul.kr  */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "builtin.h"
+
+static int	is_valid_identifier(const char *str)
+{
+	int	i;
+
+	if (!str || !*str)
+		return (NO);
+	if (!((*str >= 'a' && *str <= 'z') || (*str >= 'A' && *str <= 'Z'))
+		&& *str != '_')
+		return (NO);
+	i = 1;
+	while (str[i])
+	{
+		if (!((str[i] >= 'a' && str[i] <= 'z') 
+			|| (str[i] >= 'A' && str[i] <= 'Z')
+			|| (str[i] >= '0' && str[i] <= '9')) && str[i] != '_')
+			return (NO);
+		i++;
+	}
+	return (YES);
+}
 
 static int	key_matches(char *env_var, char *key)
 {
@@ -24,6 +45,21 @@ static int	key_matches(char *env_var, char *key)
 	return (0);
 }
 
+static void	free_env_array(char **env)
+{
+	int	i;
+
+	if (!env)
+		return ;
+	i = 0;
+	while (env[i])
+	{
+		free(env[i]);
+		i++;
+	}
+	free(env);
+}
+
 static char	**remove_env_var(char **env, char *key)
 {
 	char	**new_env;
@@ -31,8 +67,10 @@ static char	**remove_env_var(char **env, char *key)
 	int		i;
 	int		j;
 
+	if (!is_valid_identifier(key))
+		return (env);
 	count = count_env_vars(env);
-	new_env = malloc(sizeof(char *) * count);
+	new_env = malloc(sizeof(char *) * (count + 1));
 	if (!new_env)
 		return (env);
 	i = 0;
@@ -42,25 +80,42 @@ static char	**remove_env_var(char **env, char *key)
 		if (!key_matches(env[i], key))
 		{
 			new_env[j] = ft_strdup(env[i]);
+			if (!new_env[j])
+			{
+				free_env_array(new_env);
+				return (env);
+			}
 			j++;
 		}
 		i++;
 	}
 	new_env[j] = NULL;
+	free_env_array(env);
 	return (new_env);
 }
 
 int	builtin_unset(char **args, char ***env)
 {
 	int	i;
+	int	exit_code;
 
 	if (!args[1])
 		return (0);
+	exit_code = 0;
 	i = 1;
 	while (args[i])
 	{
+		if (!is_valid_identifier(args[i]))
+		{
+			write(STDERR_FILENO, "minishell: unset: `", 19);
+			write(STDERR_FILENO, args[i], ft_strlen(args[i]));
+			write(STDERR_FILENO, "': not a valid identifier\n", 26);
+			exit_code = 1;
+		}
+		else
+			*env = remove_env_var(*env, args[i]);
 		*env = remove_env_var(*env, args[i]);
 		i++;
 	}
-	return (0);
+	return (exit_code);
 }
