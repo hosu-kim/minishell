012a77cdac2682a3d7ab92b1599e700ca89a139d
Jakub Andrasi <jaandras@c1r1s5.42prague.com>
2025-06-22 17:52:58 +0200

    changes in header

--- src/builtin/env_utils.c
+++ src/builtin/env_utils.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   env_utils.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hoskim <hoskim@student.42prague.com>       +#+  +:+       +#+        */
+/*   By: jaandras <jaandras@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/19 22:25:54 by hoskim            #+#    #+#             */
-/*   Updated: 2025/06/21 02:01:42 by hoskim           ###   ########seoul.kr  */
+/*   Updated: 2025/06/22 17:46:24 by jaandras         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,20 +28,53 @@ static char	*create_env_string(char *key, char *value)
 	return (new_var);
 }
 
+int	ft_compare(const void *s1, const void *s2)
+{
+	unsigned char	*str1;
+	unsigned char	*str2;
+	size_t			i;
+
+	str1 = (void *) s1;
+	str2 = (void *) s2;
+	i = 0;
+	while (str1[i] != '=')
+	{
+		if (str1[i] != str2[i])
+			return (str1[i] - str2[i]);
+		i++;
+	}
+	return (0);
+}
+
 char	**add_env_var(char **env, char *key, char *value)
 {
 	char	**new_env;
+	int		update;
 	int		count;
 	int		i;
 
 	count = count_env_vars(env);
+	update = 0;
 	new_env = malloc(sizeof(char *) * (count + 2));
 	if (!new_env)
 		return (NULL);
 	i = -1;
 	while (++i < count)
-		new_env[i] = ft_strdup(env[i]);
-	new_env[i] = create_env_string(key, value);
-	new_env[i + 1] = NULL;
+	{
+		if (!ft_compare(env[i], key))
+		{
+			new_env[i] = create_env_string(key, value);
+			update = 1;
+		}
+		else
+			new_env[i] = ft_strdup(env[i]);
+	}
+	if (!update)
+	{
+		new_env[i] = create_env_string(key, value);
+		new_env[i + 1] = NULL;
+	}
+	else
+		new_env[i] = NULL;
 	return (new_env);
 }
